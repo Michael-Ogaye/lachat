747cf9a37d68312592a44fb04030208f1b3306c8
Michael-Ogaye <hogaeymic@gmail.com>
2023-05-14 02:17:46 +0300

    new channels

--- lachat/settings.py
+++ lachat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6hz53zydr+#!u1d%bmp4hxf*hp@v&cyzyi9z53g2m1mfpq+vjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ogchat.onrender.com']
+ALLOWED_HOSTS = ['ogchat.onrender.com','127.0.0.1']
 
 
 # Application definition
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
