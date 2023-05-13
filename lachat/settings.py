b12390c1e2fe308abea465729ba065cbf16e39d8
Michael-Ogaye <hogaeymic@gmail.com>
2023-05-14 00:47:02 +0300

    secure sockets

--- lachat/settings.py
+++ lachat/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'lachat.wsgi.application'
 ASGI_APPLICATION = "lachat.asgi.application"
 
 
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels.layers.InMemoryChannelLayer',
-       
-#     },
-# }
-
-
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis://red-chfrmkrhp8u065rihr8g", 6379)],
-        },
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+       
     },
 }
+
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("redis://red-chfrmkrhp8u065rihr8g:6379", 6379)],
+#         },
+#     },
+# }
 
 
 
