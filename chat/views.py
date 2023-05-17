999a0d00a7deaf45ad72e406c4b792c4a419d1da
devthon <ldr@gmail.com>
2023-05-17 23:42:41 +0300

    stock

--- chat/views.py
+++ chat/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.conf import settings
 
 # Create your views here.
+mode=settings.MODE
+pro='ws' if mode=='dev'else 'wss'
 
 def index(request):
    return render(request,'chat/index.html') 
 
 def roomf(request, room_name):
-    mode=settings.MODE
-    pro='ws' if mode=='dev'else 'wss'
+    
     return render(request, "chat/room.html", {'prot':pro,'room_name':room_name})
 def stockdata(request):
-    return render(request,'chat/chart.html')
+    return render(request,'chat/chart.html',{'prot':pro})
     
